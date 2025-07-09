01914ccd8134d9739154e5f7c6e18f950e03b31d
Elias <elias.lavrich@gmail.com>
2025-07-09 10:59:05 -0400

    Added a calculator for basic equations

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -14,7 +14,31 @@
             string band = Console.ReadLine();
             
             Console.WriteLine($"Hello, {name}! I am your program. I see your favorite color is {color}. I like that one too! Do you also like the {animal}? That's my favorite animal. I also heard that {band} is good, but I've never listened to them.");
+            
+            Console.WriteLine(Add(37, 47));
+            Console.WriteLine(Subtract(43, 33));
+            Console.WriteLine(Multiply(5, 38));
+            Console.WriteLine(Divide(6, 29));
         }
-        
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Divide(int a, int b)
+        {
+            return a / b;
+        }
     }
 }
